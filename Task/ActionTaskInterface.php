03374e48655ca27d4972bb8f986f218f8c11666f
Javier Hernández Gil <javibilboweb@gmail.com>
2015-01-15 22:29:53 +0100

    Add ActionTaskInterface

---
+++ Task/ActionTaskInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the build-tools-lib package.
+ *
+ * (c) Javi H. Gil <https://github.com/javihgil>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+namespace Task;
+
+/**
+ * Interface ActionTaskInterface
+ *
+ * @package  Task
+ * @author   Javi H. Gil <https://github.com/javihgil>
+ */
+interface ActionTaskInterface
+{
+    /**
+     * @return string
+     */
+    public function getAction();
+
+    /**
+     * @param string $action
+     * @return $this
+     */
+    public function setAction($action);
+}
