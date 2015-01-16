6406cb4d860d70f1430b99db25a3c8b6c10af139
Javier Hernández Gil <javibilboweb@gmail.com>
2015-01-16 08:05:40 +0100

    Add autoload.php

---
+++ autoload.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * This file is part of the tools-project package.
+ *
+ * (c) Javi H. Gil <https://github.com/javihgil>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once "Composer/ComposerJson.php";
+require_once "Composer/ComposerLock.php";
+require_once "Composer/Package.php";
+require_once "Composer/Version.php";
+require_once "Task/AbstractTask.php";
+require_once "Task/ActionTaskInterface.php";
